8f7f4155399b72aacc28bd02d3dfed32ddc51684
neil <qwe17235@gmail.com>
2024-07-27 01:39:27 +0800

    feat: Bump version to 0.1.3
    - Enhanced EventEmitter with decorator support for adding event listeners.
    - Added remove_listener and remove_all_listeners methods for managing event listeners.
    - Updated unit tests to cover new functionalities.
    - Improved documentation and examples for better clarity.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'async-pyserial'
 copyright = '2024, Neil Lei'
 author = 'Neil Lei'
-release = '0.1.2'
+release = '0.1.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
